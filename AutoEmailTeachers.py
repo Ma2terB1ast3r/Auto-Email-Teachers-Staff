22da9d18d157e62717d715338968efa5d594c0d5
Ma2terB1ast3r <jeep@juzzlea.com>
2022-08-14 15:29:48 +1000

    saves details + cleanup gui

--- AutoEmailTeachers.py
+++ AutoEmailTeachers.py
@@ -5,6 +5,7 @@ import dearpygui.dearpygui as dpg # GUI
 from exchangelib import Credentials, Account, Configuration, Message, Mailbox # MS Exchange Login
 import maskpass  # Password Hiding
 import csv # Temp for reading CSV
+from os.path import exists
 
 
 # Setting User Info
@@ -28,26 +29,104 @@ recipient = "Mr A"
 subject = "6FAIL9C"
 EmailSender = "Myself"
 
-results = []
+# Recipient Info
+RecipientEmail = []
+RecipientTitle = []
+RecipientName = []
+RecipientClass = []
 
-with open('TestTable.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader: # each row is a list
-        results.append(row)
+# CSV File Read/Write
+def read_recipients():
+    if exists("TestTable.csv"):
+        try:
+            results = []
+            global RecipientEmail
+            global RecipientTitle
+            global RecipientName
+            global RecipientClass
+            with open('TestTable.csv', 'r') as file:
+                reader = csv.reader(file)
+                for row in reader: # each row is a list
+                    results.append(row)
+            try:
+                RecipientEmail = results[0]
+                RecipientTitle = results[1]
+                RecipientName = results[2]
+                RecipientClass = results[3]
+            except IndexError:
+                pass
+        except FileNotFoundError:
+            print("error: file doesnt exist")
+            pass
+    else:
+        print("file doesnt exist")
 
-# Reading CSV
-# To be replaced with web page scraping
-RecipientEmail = results[0]
-RecipientTitle = results[1]
-RecipientName = results[2]
-RecipientClass = results[3]
+def read_details():
+    if exists("Details.csv"):
+        try:
+            with open('Details.csv', 'r') as file:
+                details = []
+                reader = csv.reader(file)
+                global ExchangeServer
+                global ExchangeEmail
+                global ExchangeUsername
+                for row in reader: # each row is a list
+                    details.append(row)
+                try:
+                    ExchangeServer = str(details[0])
+                    ExchangeEmail = str(details[1])
+                    ExchangeUsername = str(details[2])
+                    ExchangeServer = ExchangeServer.replace("[", "")
+                    ExchangeServer = ExchangeServer.replace("'", "")
+                    ExchangeServer = ExchangeServer.replace("]", "")
+                    ExchangeEmail = ExchangeEmail.replace("[", "")
+                    ExchangeEmail = ExchangeEmail.replace("'", "")
+                    ExchangeEmail = ExchangeEmail.replace("]", "")
+                    ExchangeUsername = ExchangeUsername.replace("[", "")
+                    ExchangeUsername = ExchangeUsername.replace("'", "")
+                    ExchangeUsername = ExchangeUsername.replace("]", "")
+                except IndexError:
+                    pass
+                return details
+        except FileNotFoundError:
+            print("error: file doesnt exist")
+            pass
+    else:
+        print("file doesnt exist")
+
+def save_details():
+    with open('Details.csv', 'w+') as file:
+        global ExchangeServer
+        global ExchangeEmail
+        global ExchangeUsername
+        details = []
+        details.append(ExchangeServer)
+        details.append(ExchangeEmail)
+        details.append(ExchangeUsername)
+        reader = csv.writer(file)
+        reader.writerow([details[0]])
+        reader.writerow([details[1]])
+        reader.writerow([details[2]])
 
 
+read_recipients()
+read_details()
 dpg.create_context()
 
 # UI Events
-def button_cb(sender, app_data):
-    print(ExchangeServer, ExchangeEmail, ExchangeUsername, ("*" * len(ExchangePassword)))
+def btn_readdetails():
+    global ExchangeServer
+    global ExchangeEmail
+    global ExchangeUsername
+    read_details()
+    dpg.set_value("ti_server", ExchangeServer)
+    dpg.set_value("ti_email", ExchangeEmail)
+    dpg.set_value("ti_username", ExchangeUsername)
+    print(ExchangeServer, ExchangeEmail, ExchangeUsername)
+
+def btn_savedetails():
+    save_details()
+    print(ExchangeServer, ExchangeEmail, ExchangeUsername)
 
 def ti_server_cb(sender, app_data):
     global ExchangeServer
@@ -117,6 +196,7 @@ def send_emails(sender, app_data):
 def print_me(sender):
     print(f"Menu Item: {sender}")
 
+
 # Main Window
 with dpg.window(tag="Primary Window"):
     # Top Menu
@@ -136,37 +216,35 @@ with dpg.window(tag="Primary Window"):
             dpg.add_button(label="Press Me", callback=print_me)
             dpg.add_color_picker(label="Color Me", callback=print_me)
 
-    # Table
-    with dpg.table(header_row=False, borders_innerH=True, borders_innerV=True, borders_outerH=True, borders_outerV=True, policy=dpg.mvTable_SizingFixedFit, resizable=True, no_host_extendX=True):
-        dpg.add_table_column(label="Recipient Email")
-        dpg.add_table_column(label="Recipient Title")
-        dpg.add_table_column(label="Recipient Name")
-        dpg.add_table_column(label="Recipient Class")
-
-        for i in range(len(RecipientEmail)):
-                with dpg.table_row():
-                    for j in range(4):
-                        if j == 0:
-                            dpg.add_text(RecipientTitle[i])
-                        elif j == 1:
-                            dpg.add_text(RecipientName[i])
-                        elif j == 2:
-                            dpg.add_text(RecipientClass[i])
-                        elif j == 3:
-                            dpg.add_text(RecipientEmail[i])
-                        else:
-                            continue
-
-    # First Part
-    dpg.add_text("Hello, world")
-    button1 = dpg.add_button(label="Press Me!", callback=send_emails)
-
     # Message Contents
     with dpg.tree_node(label="Message Contents", default_open=True):
+        button1 = dpg.add_button(tag="btn_sendemail", label="Send Emails", callback=send_emails)
         dpg.add_text("Subject:")
         dpg.add_input_text(tag="ti_subject", label="", default_value=EmailSubject, callback=ti_subject_cb)
         dpg.add_text("Body:")
         dpg.add_input_text(tag="ti_body", label="", default_value=EmailBody, multiline=True, height=200, width=500, tab_input=True,callback=ti_body_cb)
+    
+    # Table
+    with dpg.tree_node(label="Recipients", default_open=True):
+        with dpg.table(header_row=False, borders_innerH=True, borders_innerV=True, borders_outerH=True, borders_outerV=True, policy=dpg.mvTable_SizingFixedFit, resizable=True, no_host_extendX=True):
+            dpg.add_table_column(label="Recipient Email")
+            dpg.add_table_column(label="Recipient Title")
+            dpg.add_table_column(label="Recipient Name")
+            dpg.add_table_column(label="Recipient Class")
+
+            for i in range(len(RecipientEmail)):
+                    with dpg.table_row():
+                        for j in range(4):
+                            if j == 0:
+                                dpg.add_text(RecipientTitle[i])
+                            elif j == 1:
+                                dpg.add_text(RecipientName[i])
+                            elif j == 2:
+                                dpg.add_text(RecipientClass[i])
+                            elif j == 3:
+                                dpg.add_text(RecipientEmail[i])
+                            else:
+                                continue
 
     # Exchange Settings
     with dpg.tree_node(label="Exchange Settings", default_open=True):
@@ -178,22 +256,15 @@ with dpg.window(tag="Primary Window"):
         dpg.add_input_text(tag="ti_username", label="", default_value=ExchangeUsername, hint="Username", no_spaces=True, callback=ti_username_cb)
         dpg.add_text("Exchange password:")
         dpg.add_input_text(tag="ti_password", label="", default_value=ExchangePassword, hint="Password", no_spaces=True, password=True, callback=ti_password_cb)
+        dpg.add_button(tag="btn_save", label="Save Details", callback=btn_savedetails)
+        dpg.add_button(tag="btn_load", label="Load Details", callback=btn_readdetails)
 
 
 # DearPyGUI Create Window
-dpg.create_viewport(title='Auto Email Teachers', width=1000, height=1200)
+dpg.create_viewport(title='Auto Email Teachers', width=600, height=800)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.set_primary_window("Primary Window", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
 
-
-
-
-#EmailBodyUnique = EmailBody
-#EmailBodyUnique = EmailBodyUnique.replace("<recipient>", recipient)
-#EmailBodyUnique = EmailBodyUnique.replace("<class>", subject)
-#EmailBodyUnique = EmailBodyUnique.replace("<sender>", sender)
-
-#print(EmailBodyUnique)
